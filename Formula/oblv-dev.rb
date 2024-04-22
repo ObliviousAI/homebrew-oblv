28324bf1b7dc3b2c250106f2751db47633a82de8
Girish Joshi <mail@girishjoshi.io>
2024-04-22 18:37:15 +0530

    updated the version-names for tar.gz in the formula
    Signed-off-by: Girish Joshi <mail@girishjoshi.io>

--- Formula/oblv-dev.rb
+++ Formula/oblv-dev.rb
@@ -3,10 +3,10 @@ class OblvDev < Formula
   homepage "https://docs.oblivious.ai/cli/introduction"
 
   if Hardware::CPU.intel?
-    url "https://oblv-cli-binary.oblivious.ai/nightly/brew/oblv-ccli-0.6.0-x86_64-apple-darwin.tar.gz"
+    url "https://oblv-cli-binary.oblivious.ai/nightly/brew/oblv-ccli-0.6.0.r1-x86_64-apple-darwin.tar.gz"
     sha256 "775ca8fdd15d439bb8080f5b0fbf0e9962a353f4644a26d5913ac5689787084f"
   elsif Hardware::CPU.arm?
-    url "https://oblv-cli-binary.oblivious.ai/nightly/brew/oblv-ccli-0.6.0-aarch64-apple-darwin.tar.gz"
+    url "https://oblv-cli-binary.oblivious.ai/nightly/brew/oblv-ccli-0.6.0.r1-aarch64-apple-darwin.tar.gz"
     sha256 "f48b6f0a63c045320501f1a9346361d93811f0314a44ffdaddf647a37dbfb46f"
   end
 
