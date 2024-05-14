90a18f628b20d3b033af26a819a1ea9c72dc7147
Girish Joshi <mail@girishjoshi.io>
2024-05-14 10:37:16 +0530

    bumped up the tarball versions for oblv-dev
    Signed-off-by: Girish Joshi <mail@girishjoshi.io>

--- Formula/oblv-dev.rb
+++ Formula/oblv-dev.rb
@@ -3,10 +3,10 @@ class OblvDev < Formula
   homepage "https://docs.oblivious.ai/cli/introduction"
 
   if Hardware::CPU.intel?
-    url "https://oblv-ccli.oblivious.com/nightly/brew/oblv-ccli-0.6.0.r1-x86_64-apple-darwin.tar.gz"
+    url "https://oblv-ccli.oblivious.com/nightly/brew/oblv-ccli-0.6.0.r2-x86_64-apple-darwin.tar.gz"
     sha256 "ebe4a3ef9ad2309c42b6770573ea6d06cdd4ee9020e0ad64d1ce186fc7868cd4"
   elsif Hardware::CPU.arm?
-    url "https://oblv-ccli.oblivious.com/nightly/brew/oblv-ccli-0.6.0.r1-aarch64-apple-darwin.tar.gz"
+    url "https://oblv-ccli.oblivious.com/nightly/brew/oblv-ccli-0.6.0.r2-aarch64-apple-darwin.tar.gz"
     sha256 "79b5e70398e4e2e3c07fca858fdd0a38a20833d4448c07d866eef1b49bd1055d"
   end
 
