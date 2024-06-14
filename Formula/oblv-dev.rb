808db581071f1f5db27aae59121c49b9dc2b08df
Girish Joshi <mail@girishjoshi.io>
2024-06-14 14:27:46 +0530

    updated the formule for oblv 1.0.0
    Signed-off-by: Girish Joshi <mail@girishjoshi.io>

--- Formula/oblv-dev.rb
+++ Formula/oblv-dev.rb
@@ -3,11 +3,11 @@ class OblvDev < Formula
   homepage "https://www.oblivious.com/oblv"
 
   if Hardware::CPU.intel?
-    url "https://cli.oblivious.com/1.0.0/brew/oblv-ccli-1.0.0-x86_64-apple-darwin.tar.gz"
-    sha256 "dbe1369b358b058c812774a6b7c3e774bbec4db52c67031927e8a3d405a875a3"
+    url "https://cli.oblivious.com/1.0.0/brew/oblv-ccli-1.0.0-r1-x86_64-apple-darwin.tar.gz"
+    sha256 "d4799c93f036c8492bbf95c7bf3e7473f5e22a017baff32b0c9282e51d9cf825"
   elsif Hardware::CPU.arm?
-    url "https://cli.oblivious.com/1.0.0/brew/oblv-ccli-1.0.0-aarch64-apple-darwin.tar.gz"
-    sha256 "4c7e93d5a586815f5ce738349b81b98f5e270f31bc1b9855b51b114702dbb970"
+    url "https://cli.oblivious.com/1.0.0/brew/oblv-ccli-1.0.0-r1-aarch64-apple-darwin.tar.gz"
+    sha256 "31b1f8d34c9dcecfd42d7ac774b252510d2ce58ca9de72c716523c16fa570cc6"
   end
 
   license ""
