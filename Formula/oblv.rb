50f7a229247303250204ce815c78fc7bfbf53262
Girish Joshi <mail@girishjoshi.io>
2025-06-11 12:12:26 +0530

    updated the release url and the home page url in oblv formula
    Signed-off-by: Girish Joshi <mail@girishjoshi.io>

--- Formula/oblv.rb
+++ Formula/oblv.rb
@@ -1,12 +1,12 @@
 class Oblv < Formula
   desc "Brew formula for oblv client proxy"
-  homepage "https://www.oblivious.com/oblv"
+  homepage "https://www.oblivious.com/oblv-deploy"
 
   if Hardware::CPU.intel?
-    url "https://cli.oblivious.com/dev/brew/oblv-ccli-1.2.1-r0-x86_64-apple-darwin.tar.gz"
+    url "https://cli.oblivious.com/latest/brew/oblv-ccli-1.2.1-r0-x86_64-apple-darwin.tar.gz"
     sha256 "ea1fad3c563ce8cba5a268970da221b8220ca475bf6490944d335e7dbf3ec333"
   elsif Hardware::CPU.arm?
-    url "https://cli.oblivious.com/dev/brew/oblv-ccli-1.2.1-r0-aarch64-apple-darwin.tar.gz"
+    url "https://cli.oblivious.com/latest/brew/oblv-ccli-1.2.1-r0-aarch64-apple-darwin.tar.gz"
     sha256 "f00eae664aaf47e81964c05764c5fa11c8deae6d3a23e17ec93a0a4130cc84a4"
   end
 
